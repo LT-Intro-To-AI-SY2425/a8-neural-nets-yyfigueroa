35c13e624dd6ef3c27b7ff2f155353e4132343e6
Yoaly <yyfigueroa@cps.edu>
2025-04-24 11:24:43 -0500

    done

--- a8.py
+++ a8.py
@@ -14,5 +14,27 @@ xor_training_data = [([1, 1], [0]), ([1, 0], [1]), ([0, 1], [1]), ([0, 0], [0])]
 xorn = NeuralNet(2, 1, 1)
 xorn.train(xor_training_data)
 print(xorn.test_with_expected(xor_training_data))
+print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
+print("\n\nTraining XOR\n\n")
+xor_training_data = [
+    ([1, 1], [0]), 
+    ([1, 0], [1]), 
+    ([0, 1], [1]), 
+    ([0, 0], [0])
+    ]
 
+xorn = NeuralNet(2, 2, 1)
+xorn.train(xor_training_data, learning_rate = .7, iters =10000, print_interval = 1000)
+print(xorn.test_with_expected(xor_training_data))
+
+print("\n\nTraining XOR\n\n")
+xor_training_data = [
+    ([0.9, 0.6, 0.8, 0.3, 0.1],[1])
+    ([0.8, 0.8, 0.4, 0.6, 0.4],[1])
+    ([0.7, 0.2, 0.4, 0.6, 0.3], [1])
+    ([0.5, 0.5, 0.8, 0.4, 0.8], [0])
+]
+xorn = NeuralNet(2,1,1)
+xorn.train(xor_training_data, iters=10000)
+print(xorn.test_with_expected(xor_training_data))
 
